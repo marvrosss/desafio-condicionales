53dac4f1571becc4ec7dc1a12e6aaef29ed1b70c
marvrosss <marvrosss@gmail.com>
2023-10-23 23:03:31 -0300

    1er desafio de 3

---
+++ accets/js/js.js
@@ -0,0 +1,12 @@
+var carro = document.querySelector("#carro")
+
+function activarborde() {
+
+    if (carro.style.border) {
+        carro.style.border = "none"
+    }
+    else {
+        carro.style.border = "solid 2px red"
+    }
+    setInterval(activarborde, 10000);
+}
